62c314459a19d4a0a1c98ac5406f28747ef2a23f
Shubham <theshubham.dev@gmail.com>
2023-09-22 01:38:18 +0530

    github oath setup

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center h-screen gap-10">
       <h1>Welcome to Image Craft Ai</h1>
       <Link href="/auth">signin to explore</Link>
     </div>

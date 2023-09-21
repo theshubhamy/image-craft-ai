7c35d28faf2343132ed2f52f7c3fac082ab62718
Shubham <theshubham.dev@gmail.com>
2023-09-22 01:36:18 +0530

    github oath setup

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -20,9 +20,11 @@ export default function CamperVanPage() {
   }
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center h-screen gap-10">
       <p>Not signed in.</p>
-      <button onClick={() => signIn('github')}>Sign in</button>
-    </>
+      <button onClick={() => signIn('github')}>
+        Sign in with Gihub
+      </button>
+    </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to Image Craft Ai</h1>
+      <Link href="/auth">signin to explore</Link>
+    </div>
+  );
 };
 
 export default Home;
